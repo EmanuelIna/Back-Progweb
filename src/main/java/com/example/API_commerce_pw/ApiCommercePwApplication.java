661694a23398c46eeeed9493395066d3c8d9fa51
EmanuelIna <emanuelinacio052@gmail.com>
2026-09-11 20:35:07 -0400

    configuracao basica de ambiente com dependencias iniciais

---
+++ src/main/java/com/example/API_commerce_pw/ApiCommercePwApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_commerce_pw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCommercePwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCommercePwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API_commerce_pw/ApiCommercePwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_commerce_pw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCommercePwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
